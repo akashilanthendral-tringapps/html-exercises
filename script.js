d8b652085837c24b22b85ed3f5a056af3a519017
akashilanthendral-tringapps <akash.ilanthendral@tringapps.com>
2021-12-17 19:15:34 +0530

    commit 1

---
+++ script.js
@@ -0,0 +1,10 @@
+// const items = document.querySelectorAll(".item");
+// items.forEach((item) => {
+//     console.log(item);
+// })
+const items = document.querySelectorAll('.item')
+items.forEach((item) => {
+    console.log(item.innerText)
+})
+
+// item.remove();
